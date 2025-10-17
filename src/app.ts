98e4c89c7a83ed48df6d8db3845179b89f9e29f0
Kyaw Soe <kyawsoedeveloper@gmail.com>
2025-10-17 14:15:13 +0630

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,27 @@
 import express from "express";
+import cors from "cors";
 import routes from "./routes/route";
 import sentryDashboard from "./routes/sentry";
 import { Sentry } from "./config/sentry";
 
 const app = express();
 
+const allowedOrigin = process.env.ALLOW_URL || "http://localhost:3000";
+
+app.use(
+  cors({
+    origin: allowedOrigin,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", routes);
-
 app.use("/dashboard", sentryDashboard);
 
 Sentry.setupExpressErrorHandler(app);
-
 app.use(Sentry.expressErrorHandler());
 
 app.use(
